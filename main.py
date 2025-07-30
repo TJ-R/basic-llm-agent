be4b74064931bcb132f320fe34e083993d8c4187
Thomas Runyan <runyant13@gmail.com>
2025-07-30 19:01:15 -0400

    Added input from command and preping for keeping context between questions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 
@@ -10,12 +12,25 @@ def main():
     client = genai.Client(api_key=api_key)
 
     model  = "gemini-2.0-flash-001"
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    resp = client.models.generate_content(model=model, contents=prompt)
+    messages = []
 
-    print(f"Response: {resp.text}")
-    print(f"Prompt tokens: {resp.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {resp.usage_metadata.candidates_token_count}")
+    if len(sys.argv) > 1:
+        prompt = sys.argv[1]
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=prompt)]),
+        ]
+
+    else:
+        print("Error: no prompt provided.")
+        sys.exit(1)
+
+    resp = client.models.generate_content(model=model, contents=messages)
+
+
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        print(f"User prompt: {resp.text}")
+        print(f"Prompt tokens: {resp.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {resp.usage_metadata.candidates_token_count}")
 
 
 
