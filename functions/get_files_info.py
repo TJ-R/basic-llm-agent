15676b36d202edc2be0dd058f09fc95543d4fccc
Thomas Runyan <runyant13@gmail.com>
2025-07-31 22:22:50 -0400

    Added function to get all files in directory

---
+++ functions/get_files_info.py
@@ -0,0 +1,24 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    abs_working_dir = os.path.abspath(working_directory) 
+    target_dir = os.path.abspath(os.path.join(working_directory, directory))
+
+    if not target_dir.startswith(abs_working_dir):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
+
+    try:
+        dir_content = os.listdir(target_dir)
+        content = [] 
+        for item in dir_content:
+            join_item_path = os.path.join(target_dir, item)
+            size = os.path.getsize(join_item_path)
+            is_dir = os.path.isdir(join_item_path)
+            content.append(f"- {item}: file_size={size}, is_dir={is_dir}") 
+    except Exception as e:
+        return f'Error: {e}'
+
+    return "\n".join(content)
